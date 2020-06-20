78ff8d3fe78313728aa0fe67f09c4520e8adeb88
Seti-0 <kcosson111@gmail.com>
2020-06-20 13:53:50 +0100

    Initial Setup

---
+++ MiniRacing/Source/Code/CorePlugin/CorePlugin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality;
+
+namespace Soulstone.Duality.Plugins.MiniRacing
+{
+	public class MiniRacingPlugin : CorePlugin { }
+}
